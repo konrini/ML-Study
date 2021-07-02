c99c983648cacef8ba61793d8f5e1961b2565c9c
Migon <migon99@naver.com>
2021-07-02 10:13:56 +0900

    Computer Vision Practice

--- 2021-06-25 change_colors.py
+++ 2021-06-25 change_colors.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 heart = cv2.imread('C:/Users/user/vision/data/Heart10.jpg')
@@ -7,8 +8,15 @@ heart = cv2.imread('C:/Users/user/vision/data/Heart10.jpg')
 # 그레이스케일로 색상공간 변환
 heart_gray = cv2.cvtColor(heart, cv2.COLOR_RGB2GRAY)
 
+# 임계값으로 적합한 값 고르기
+# hist = cv2.calcHist([heart_gray], [0], None, [256], [0, 256])
+# print(plt.bar(range(256), hist.flatten()))
+
 # 128을 임계값으로 이진화
-_, frame = cv2.threshold(heart_gray, 128, 255, cv2.THRESH_BINARY_INV)
+_, frame = cv2.threshold(heart_gray, 128, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+# 잡음을 줄이기 위한 닫기 연산 (팽창 후 침식)
+frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, None, iterations=1)
 
 # 경계값 찾기
 contours, hierarchy = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -26,4 +34,3 @@ cv2.imshow('colorful_heart', colorful_heart)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
-
